5e0985b3aef0348a838826237392476c76f13057
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-22 07:58:55 +0530

    refactor: Convert ProductResponse record into class and add necessary annotations - Using record feature tried to handle dto (response) but tests are failing with it.

--- db-h2-connection/src/main/java/com/spring/h2/dto/ProductResponse.java
+++ db-h2-connection/src/main/java/com/spring/h2/dto/ProductResponse.java
@@ -1,14 +1,20 @@
 package com.spring.h2.dto;
 
 import com.spring.h2.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record ProductResponse(
-        int productId,
-        String productName,
-        String description,
-        double productPrice,
-        int productQuantity
-){
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse{
+    private int id;
+    private String productName;
+    private String description;
+    private double productPrice;
+    private int productQuantity;
+
     public static ProductResponse toProductResponse(Product product) {
         return new ProductResponse(
                 product.getId(),
