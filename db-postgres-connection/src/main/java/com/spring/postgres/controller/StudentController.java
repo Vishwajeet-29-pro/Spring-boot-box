ec7d2ec8cc66092ae8b26d7222534ee2d76c8894
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-24 08:17:38 +0530

    feat: Add code for GET mapping to return list of students

--- db-postgres-connection/src/main/java/com/spring/postgres/controller/StudentController.java
+++ db-postgres-connection/src/main/java/com/spring/postgres/controller/StudentController.java
@@ -6,10 +6,9 @@ import com.spring.postgres.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
@@ -21,5 +20,10 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<StudentResponse> saveStudent(@RequestBody StudentRequest studentRequest) {
         return new ResponseEntity<>(studentService.saveStudent(studentRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResponse>> findAllStudents() {
+        return ResponseEntity.ok(studentService.findAllStudents());
     }
 }
