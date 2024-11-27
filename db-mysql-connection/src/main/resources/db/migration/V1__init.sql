628d221e255443f63a925f22cd5a8a478ac97319
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-27 21:35:24 +0530

    feat(database): Add version file for database migration

---
+++ db-mysql-connection/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    customer_address VARCHAR(255) NOT NULL
+);
