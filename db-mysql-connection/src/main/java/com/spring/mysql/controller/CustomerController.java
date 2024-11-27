9a58a86acd34e274719a854447eb8293fafc5721
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-27 19:15:18 +0530

    feat: Add POST method and make test pass

--- db-mysql-connection/src/main/java/com/spring/mysql/controller/CustomerController.java
+++ db-mysql-connection/src/main/java/com/spring/mysql/controller/CustomerController.java
@@ -1,7 +1,13 @@
 package com.spring.mysql.controller;
 
+import com.spring.mysql.dto.CustomerRequest;
+import com.spring.mysql.dto.CustomerResponse;
 import com.spring.mysql.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerResponse> createCustomerAccount(@RequestBody CustomerRequest customerRequest) {
+        CustomerResponse customerResponse = customerService.createCustomerAccount(customerRequest);
+        return new ResponseEntity<>(customerResponse, HttpStatus.CREATED);
+    }
 }
