362ba7f18acfc7ca11b8e5a42619324431528eba
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-28 21:49:29 +0530

    fix: Return type of find by id method

--- db-mongo-connection/src/main/java/com/spring/mongo/service/UserProfileService.java
+++ db-mongo-connection/src/main/java/com/spring/mongo/service/UserProfileService.java
@@ -4,11 +4,12 @@ import com.spring.mongo.dto.UserProfileRequest;
 import com.spring.mongo.dto.UserProfileResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserProfileService {
     UserProfileResponse createProfile(UserProfileRequest userProfileRequest);
     List<UserProfileResponse> findAllUserProfile();
-    UserProfileResponse findUserProfileById(String id);
+    Optional<UserProfileResponse> findUserProfileById(String id);
     UserProfileResponse updateUserProfileById(String id, UserProfileRequest userProfileRequest);
     void deleteUserProfileById(String id);
 }
