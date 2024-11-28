e98ee99f4ea7139de3dc0e74436bfc9f457bb9b5
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-28 20:46:45 +0530

    feat: Add Data model for mongodb implementation

---
+++ db-mongo-connection/src/main/java/com/spring/mongo/model/UserProfile.java
@@ -0,0 +1,22 @@
+package com.spring.mongo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "user_profiles")
+public class UserProfile {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String email;
+    private String phone;
+    private boolean isActive;
+}
