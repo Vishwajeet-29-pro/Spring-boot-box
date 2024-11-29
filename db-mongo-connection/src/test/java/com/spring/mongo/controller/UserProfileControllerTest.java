b5b7352ee8bbd664973de5cbe0a51adc7ec41972
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-29 20:24:16 +0530

    test: Add test for POST request

--- db-mongo-connection/src/test/java/com/spring/mongo/controller/UserProfileControllerTest.java
+++ db-mongo-connection/src/test/java/com/spring/mongo/controller/UserProfileControllerTest.java
@@ -1,13 +1,24 @@
 package com.spring.mongo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.mongo.dto.UserProfileRequest;
 import com.spring.mongo.dto.UserProfileResponse;
 import com.spring.mongo.service.UserProfileService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserProfileController.class)
 class UserProfileControllerTest {
@@ -27,6 +38,16 @@ class UserProfileControllerTest {
         profileResponse = new UserProfileResponse("1234", "johndoe", "john.doe@springbox.com","9876543210",false);
     }
 
+    @Test
+    public void create_user_profile_should_return_user_profile_and_status_code_201() throws Exception {
+        when(userProfileService.createProfile(any(UserProfileRequest.class))).thenReturn(profileResponse);
 
+        mockMvc.perform(post("/api/user-profile")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(profileRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username").value("johndoe"))
+                .andExpect(jsonPath("$.active", Matchers.is(false)));
+    }
 
 }
