c56d9666272e91aaefef8a9bb41edc432d4ebc01
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-29 20:24:36 +0530

    feat: Add POST mapping code and make test pass

--- db-mongo-connection/src/main/java/com/spring/mongo/controller/UserProfileController.java
+++ db-mongo-connection/src/main/java/com/spring/mongo/controller/UserProfileController.java
@@ -1,7 +1,13 @@
 package com.spring.mongo.controller;
 
+import com.spring.mongo.dto.UserProfileRequest;
+import com.spring.mongo.dto.UserProfileResponse;
 import com.spring.mongo.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
+
+    @PostMapping
+    public ResponseEntity<UserProfileResponse> createUserProfile(@RequestBody UserProfileRequest userProfileRequest) {
+        UserProfileResponse userProfileResponse = userProfileService.createProfile(userProfileRequest);
+        return new ResponseEntity<>(userProfileResponse, HttpStatus.CREATED);
+    }
 }
