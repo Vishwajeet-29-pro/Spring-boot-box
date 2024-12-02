bda102811c0d920123bac102df2ff83b0d695697
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-02 22:18:36 +0530

    fix: Wrong test code and make test correctly pass

--- db-mongo-connection/src/test/java/com/spring/mongo/service/UserProfileServiceTest.java
+++ db-mongo-connection/src/test/java/com/spring/mongo/service/UserProfileServiceTest.java
@@ -107,8 +107,9 @@ class UserProfileServiceTest {
     @Test
     public void delete_user_by_id_should_delete_user_profile() {
         String id = "1";
+        when(userProfileRepository.existsById(id)).thenReturn(true);
         doNothing().when(userProfileRepository).deleteById(id);
-        userProfileRepository.deleteById(id);
+        userProfileService.deleteUserProfileById(id);
 
         verify(userProfileRepository, times(1)).deleteById(id);
     }
