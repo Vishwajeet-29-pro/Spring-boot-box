81150d6523cb2a4d637a4f6d620ebc86d08b214e
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-05 21:29:11 +0530

    feat: Add controller code for list of all users, when role is admin

--- spring-security-basic/src/main/java/com/spring/security/UserController.java
+++ spring-security-basic/src/main/java/com/spring/security/UserController.java
@@ -1,12 +1,27 @@
 package com.spring.security;
 
+import com.spring.security.dto.UserResponse;
+import com.spring.security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserResponse>> listAllUsers() {
+        return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
 }
