03a2fff2a2fcba73f323fb5409ab300740d6460f
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-05 20:59:38 +0530

    feat: Add controller layer class with Test class

---
+++ spring-security-basic/src/test/java/com/spring/security/UserControllerTest.java
@@ -0,0 +1,19 @@
+package com.spring.security;
+
+import com.spring.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
+
+}
