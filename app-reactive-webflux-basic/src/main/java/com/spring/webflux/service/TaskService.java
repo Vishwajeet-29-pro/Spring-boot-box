87c0314683f8351d0873c8c0170e37b194bd15f7
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-21 08:49:42 +0530

    fix: Fix the return type of findAll method

--- app-reactive-webflux-basic/src/main/java/com/spring/webflux/service/TaskService.java
+++ app-reactive-webflux-basic/src/main/java/com/spring/webflux/service/TaskService.java
@@ -5,12 +5,10 @@ import com.spring.webflux.dto.TaskResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 public interface TaskService {
 
     Mono<TaskResponse> createTask(TaskRequest taskRequest);
-    Flux<List<TaskResponse>> findAll();
+    Flux<TaskResponse> findAll();
     Mono<TaskResponse> findTaskById(Long id);
     Mono<TaskResponse> updateTaskById(Long id, TaskRequest taskRequest);
     Mono<?> deleteTaskById(Long id);
