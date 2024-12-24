6a0c6e7d8195b56dfd7e4f567bc347ca36593238
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-24 22:28:15 +0530

    fix: Remove decrypted MockBean annotation and replace with MockitoBean

--- app-spring-security-basic/src/test/java/com/spring/security/controller/UserControllerTest.java
+++ app-spring-security-basic/src/test/java/com/spring/security/controller/UserControllerTest.java
@@ -10,10 +10,10 @@ import com.spring.security.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -29,7 +29,7 @@ class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
+    @MockitoBean
     private UserService userService;
 
     @Test
