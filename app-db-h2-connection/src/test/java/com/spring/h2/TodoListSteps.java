014f2a2a1bc5009488f8de5a224e5f26349deac7
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-25 22:16:05 +0530

    test: Add logic in test

--- app-db-h2-connection/src/test/java/com/spring/h2/TodoListSteps.java
+++ app-db-h2-connection/src/test/java/com/spring/h2/TodoListSteps.java
@@ -1,22 +1,38 @@
 package com.spring.h2;
 
+import com.spring.h2.model.Todo;
+import com.spring.h2.repository.TodoRepository;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoListSteps {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    private Todo todo;
+
     @Given("I have an empty todo list")
     public void iHaveAnEmptyTodoList() {
-
+        todoRepository.deleteAll();
     }
 
     @When("I add a task {string}")
     public void iAddATask(String item) {
-
+        todo = new Todo();
+        todo.setTodo(item);
+        todo.setComplete(false);
+        todoRepository.save(todo);
     }
 
     @Then("the task {string} should be in the todo list")
     public void theTaskShouldBeInTheTodoList(String item) {
+        Todo savedTodo = todoRepository.findByTodo(item);
+        assertEquals(item, savedTodo.getTodo());
 
     }
 }
