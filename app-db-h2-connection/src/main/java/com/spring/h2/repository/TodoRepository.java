ea18ddc955b32e6ecbedfa525e02ef760f7f195c
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-25 22:03:24 +0530

    feat: Add custom method to find Todo item by name itself

--- app-db-h2-connection/src/main/java/com/spring/h2/repository/TodoRepository.java
+++ app-db-h2-connection/src/main/java/com/spring/h2/repository/TodoRepository.java
@@ -4,4 +4,5 @@ import com.spring.h2.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    Todo findByTodo(String item);
 }
