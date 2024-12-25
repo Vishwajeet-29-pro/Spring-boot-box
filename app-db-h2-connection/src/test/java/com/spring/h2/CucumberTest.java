0671d4e2761bafa8596ecb981558ed10d97b7650
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-25 22:16:46 +0530

    test: Add annotation `@SpringBootTest` and make test pass

--- app-db-h2-connection/src/test/java/com/spring/h2/CucumberTest.java
+++ app-db-h2-connection/src/test/java/com/spring/h2/CucumberTest.java
@@ -4,10 +4,12 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @CucumberContextConfiguration
+@SpringBootTest
 public class CucumberTest {
 }
