f932231e7b6c28daab916fd960f75b123d8ec521
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-25 19:53:08 +0530

    build: Add dependencies for BDD support

--- app-db-h2-connection/build.gradle.kts
+++ app-db-h2-connection/build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.cucumber:cucumber-java:7.20.1")
+    testImplementation("io.cucumber:cucumber-spring:7.20.1")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:7.20.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
