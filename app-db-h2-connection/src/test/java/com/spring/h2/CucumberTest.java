c09e721aaf554a8655b64db2a304f0d43252c667
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-27 08:30:42 +0530

    fix: Add properties in @SpringBootTest to make tests pass

--- app-db-h2-connection/src/test/java/com/spring/h2/CucumberTest.java
+++ app-db-h2-connection/src/test/java/com/spring/h2/CucumberTest.java
@@ -6,10 +6,12 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @CucumberContextConfiguration
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"spring.main.allow-bean-definition-overriding=true"})
 public class CucumberTest {
 }
