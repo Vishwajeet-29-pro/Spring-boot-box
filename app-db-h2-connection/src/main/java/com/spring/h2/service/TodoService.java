c4d604f9150969f9b2618b6fdbd4804648bd2c7b
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-29 15:30:54 +0530

    feat: Add GET all method in TodoService

--- app-db-h2-connection/src/main/java/com/spring/h2/service/TodoService.java
+++ app-db-h2-connection/src/main/java/com/spring/h2/service/TodoService.java
@@ -3,7 +3,10 @@ package com.spring.h2.service;
 import com.spring.h2.dto.TodoRequest;
 import com.spring.h2.dto.TodoResponse;
 
+import java.util.List;
+
 public interface TodoService {
     TodoResponse saveTodo(TodoRequest todoRequest);
     TodoResponse getTodoById(Long id);
+    List<TodoResponse> retrieveAll();
 }
