6cafc1b3eaec1aff57f18009faf173ab973e134d
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-08 08:13:55 +0530

    test: Add test for create employee

--- app-db-relationships/src/test/java/com/spring/relationship/one_to_one/service/EmployeeServiceTest.java
+++ app-db-relationships/src/test/java/com/spring/relationship/one_to_one/service/EmployeeServiceTest.java
@@ -1,5 +1,7 @@
 package com.spring.relationship.one_to_one.service;
 
+import com.spring.relationship.one_to_one.dto.EmployeeRequest;
+import com.spring.relationship.one_to_one.dto.EmployeeResponse;
 import com.spring.relationship.one_to_one.entity.Employee;
 import com.spring.relationship.one_to_one.entity.ParkingSpot;
 import com.spring.relationship.one_to_one.repository.EmployeeRepository;
@@ -11,7 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
@@ -35,6 +40,14 @@ class EmployeeServiceTest {
 
     @Test
     void test_create_employee_should_add_employee_details() {
-        assertTrue(true);
+        EmployeeRequest employeeRequest = new EmployeeRequest("Vishwajeet", "vishwajeet@springbox.com", null);
+        when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
+
+        EmployeeResponse employeeResponse = employeeService.createEmployee(employeeRequest);
+
+        assertNotNull(employeeRequest);
+        assertThat(employeeResponse.getEmployeeName()).isEqualTo(employee.getEmployeeName());
+        assertThat(employeeResponse.getEmail()).isEqualTo(employee.getEmail());
+        verify(employeeRepository, times(1)).save(any(Employee.class));
     }
 }
