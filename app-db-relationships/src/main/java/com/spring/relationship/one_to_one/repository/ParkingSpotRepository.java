4e0752d46462c2e467fb17059af4ff19a4e0306e
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-16 08:17:22 +0530

    fix: Replace correct entity class for JpaRepository Generic type

--- app-db-relationships/src/main/java/com/spring/relationship/one_to_one/repository/ParkingSpotRepository.java
+++ app-db-relationships/src/main/java/com/spring/relationship/one_to_one/repository/ParkingSpotRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ParkingSpotRepository extends JpaRepository<ParkingSpotRepository, Long> {
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
     Optional<ParkingSpot> findBySpotNumber(String spotNumber);
 }
