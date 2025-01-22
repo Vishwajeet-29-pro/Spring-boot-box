83806bd4bff08bd9458986d45167bf819c2bf33b
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-22 08:12:39 +0530

    test: Add test for create parking spot

--- app-db-relationships/src/test/java/com/spring/relationship/one_to_one/service/ParkingSpotServiceTest.java
+++ app-db-relationships/src/test/java/com/spring/relationship/one_to_one/service/ParkingSpotServiceTest.java
@@ -1,15 +1,20 @@
 package com.spring.relationship.one_to_one.service;
 
+import com.spring.relationship.one_to_one.dto.ParkingSpotRequest;
+import com.spring.relationship.one_to_one.dto.ParkingSpotResponse;
 import com.spring.relationship.one_to_one.entity.Employee;
 import com.spring.relationship.one_to_one.entity.ParkingSpot;
 import com.spring.relationship.one_to_one.repository.EmployeeRepository;
 import com.spring.relationship.one_to_one.repository.ParkingSpotRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ParkingSpotServiceTest {
@@ -31,4 +36,16 @@ class ParkingSpotServiceTest {
         employee = new Employee(1L, "Vishwajeet", "vishwajeet@springbox.com", null);
     }
 
+    @Test
+    void create_parking_spot_should_return_parking_spot_response() {
+        ParkingSpotRequest parkingSpotRequest = new ParkingSpotRequest("A1", false, null);
+        when(parkingSpotRepository.save(any(ParkingSpot.class))).thenReturn(parkingSpot);
+
+        ParkingSpotResponse parkingSpotResponse = parkingSpotService.createParkingSpot(parkingSpotRequest);
+
+        assertNotNull(parkingSpotResponse);
+        assertEquals(parkingSpotRequest.getSpotNumber(), parkingSpotResponse.getSpotNumber());
+        assertFalse(parkingSpotResponse.isAssigned());
+        assertNull(parkingSpotResponse.getEmployeeId());
+    }
 }
