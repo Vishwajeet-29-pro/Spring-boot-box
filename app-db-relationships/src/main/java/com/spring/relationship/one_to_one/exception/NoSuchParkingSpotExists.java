a15a4218862772a94c72f3a6a173ba3e93248223
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-22 08:30:34 +0530

    fix: Pass message to parent class via super()

--- app-db-relationships/src/main/java/com/spring/relationship/one_to_one/exception/NoSuchParkingSpotExists.java
+++ app-db-relationships/src/main/java/com/spring/relationship/one_to_one/exception/NoSuchParkingSpotExists.java
@@ -2,5 +2,6 @@ package com.spring.relationship.one_to_one.exception;
 
 public class NoSuchParkingSpotExists extends RuntimeException {
     public NoSuchParkingSpotExists(String message) {
+        super(message);
     }
 }
